be1fc60921efd09dadfc35dd7446a04546012b58
Mr. Is <isaac.ardis@gmail.com>
2016-01-08 12:36:46 -0500

    IntAssignmentTester class

---
+++ src/module2/IntAssignmentTester.java
@@ -0,0 +1,15 @@
+package module2;
+
+public class IntAssignmentTester {
+
+	public static void main(String[] args) {
+		
+		int var1 = 17;
+		int var2 = var1 + 9;
+		var1 = var2 + 41;
+		System.out.println("var1: " + var1 + ", var2: " + var2);
+		
+
+	}
+
+}
