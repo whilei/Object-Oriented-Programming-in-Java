37bf5f914d3ec301b9b338d6daafa165b71ced3d
Mr. Is <isaac.ardis@gmail.com>
2016-01-11 11:55:13 -0500

    Finish module 3 assignment; plot earthquakes with markers based on magnitude on location.

--- src/module3/EarthquakeCityMap.java
+++ src/module3/EarthquakeCityMap.java
@@ -53,14 +53,14 @@ public class EarthquakeCityMap extends PApplet {
 
 	
 	public void setup() {
-		size(950, 600, OPENGL);
+		size(950, 700, OPENGL);
 
 		if (offline) {
 		    map = new UnfoldingMap(this, 200, 50, 700, 500, new MBTilesMapProvider(mbTilesString));
 		    earthquakesURL = "2.5_week.atom"; 	// Same feed, saved Aug 7, 2015, for working offline
 		}
 		else {
-			map = new UnfoldingMap(this, 200, 50, 700, 500, new Google.GoogleMapProvider());
+			map = new UnfoldingMap(this, 50, 85, 700, 500, new Google.GoogleMapProvider());
 			// IF YOU WANT TO TEST WITH A LOCAL FILE, uncomment the next line
 			//earthquakesURL = "2.5_week.atom";
 		}
@@ -110,10 +110,48 @@ public class EarthquakeCityMap extends PApplet {
 //		return new SimplePointMarker(feature.getLocation());
 		
 		// http://unfoldingmaps.org/javadoc/de/fhpotsdam/unfolding/marker/SimplePointMarker.html
-		SimplePointMarker spoint = new SimplePointMarker(feature.getLocation()); 
-		spoint.setRadius((float) 10.0); 
+		SimplePointMarker spoint = new SimplePointMarker(feature.getLocation());  
+		
+		Object magnitudeObj = feature.getProperty("magnitude");
+		float magnitude = Float.parseFloat(magnitudeObj.toString());
+		// see what our range is like
+//			System.out.println(magnitude); // 2.5 - 6, call it 8
+		// remember we have predefined THRESHHOLDs above, and that feed is only for 2.5+
+		
+		// Modify marker to show earthquake magnitude.
+
+		// radius
+		float magRad = magnitude * 2; // set radius to depend on magnitude
+		spoint.setRadius(magRad); 
+
+		// color
+			// should be between 10-255, given 2.5-8. i sense a map.
+			// I want light red for minimal, medium red for medium, and dark red for big deal quakes
+		
+		float colorLevel = map(magnitude, (float) 2.5, (float) 8, (float) 10, (float) 255); // map 2.5-8 => 10-255
+		
 		// http://unfoldingmaps.org/tutorials/markers-simple.html
-		map.addMarker(spoint);
+		spoint.setColor(color(colorLevel, 0, 0, colorLevel)); // (that fourth arg is for the alpha value, which should be between 0 (transparent) and 1.0 (opaque) or, as in this case :: 0-255 
+		spoint.setStrokeColor(color(255, 0, 0, 0)); // transparent stroke (= outline)
+//		spoint.setStrokeWeight(4);
+		
+		// Now, if you wanted to actually follow the rules and fulfill assignment requirement #4, where "as a minimum you should have the size and color change for at least 3 different levels of earthquake", then instead of the directly above, you'd want to set threshhold-based if: 
+		// light
+		if (magnitude <= THRESHOLD_LIGHT) {
+			// spoint.setColor(color(0, colorLevel, colorLevel, colorLevel));
+		}
+		// moderate
+		if (magnitude >= THRESHOLD_LIGHT && magnitude < THRESHOLD_MODERATE) {
+			// spoint.setColor(color(colorLevel, 0, 0, colorLevel));
+		} 
+		// heavy
+		if (magnitude >= THRESHOLD_MODERATE) {
+			spoint.setStrokeColor(color(255, 0, 0, colorLevel));
+			spoint.setStrokeWeight(Math.round(magnitude)); // round float to int
+		}
+		
+		
+		map.addMarker(spoint); // add each marker to map (only once) 
 		return spoint;
 	}
 	
@@ -129,6 +167,9 @@ public class EarthquakeCityMap extends PApplet {
 	private void addKey() 
 	{	
 		// Remember you can use Processing's graphics methods here
+//		rect(50, 50, 100, 200, 7); // 7 is border radius i assume
+		text("Color and opacity correspond to charted magnitude of earthquake.", 55, 55);
+		text("Earthquakes catalogued as greater than moderate (> " + THRESHOLD_MODERATE + ") are outlined in a border also corresponding to their relative magnitude.", 55, 70);
 	
 	}
 }
